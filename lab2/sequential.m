2b9316251ca2b6c274b3ba9608138ccee021033a
Will Bradbeer <willbradbeer@gmail.com>
2017-03-14 15:57:12 -0400

    Fixed size vs length bug.

--- lab2/sequential.m
+++ lab2/sequential.m
@@ -4,8 +4,8 @@ function classifier = sequential(a, b)
         G_good = false;
         while(~G_good)
             % select random points from a and b
-            rand_a = randi([1,length(a)], 1);
-            rand_b = randi([1,length(b)], 1);
+            rand_a = randi([1,size(a, 1)], 1);
+            rand_b = randi([1,size(b, 1)], 1);
             a_r = a(rand_a, :);
             b_r = b(rand_b, :);
             % MED classifier for points
@@ -14,12 +14,12 @@ function classifier = sequential(a, b)
             c3 = (a_r(1)^2 + a_r(2)^2 - b_r(1)^2 - b_r(2)^2)/2;
             G = [c1 c2 c3];
             % check classifier against points
-            class_a = zeros(1, length(a));
+            class_a = zeros(1, size(a, 1));
             for i=1:size(a, 1)
                 class_a(i) = applyDisc(G, a(i, :));
             end
             a_wrong = sum(class_a == 2);
-            class_b = zeros(1, length(b));
+            class_b = zeros(1, size(b, 1));
             for i=1:size(b, 1)
                 class_b(i) = applyDisc(G, b(i, :));
             end
