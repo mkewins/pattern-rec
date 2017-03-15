bf7d59b5481dd668bfbfed47e0391b201db5e0c8
Will Bradbeer <willbradbeer@gmail.com>
2017-03-15 09:51:35 -0400

    Fixed classify.

--- lab2/classify.m
+++ lab2/classify.m
@@ -1,14 +1,14 @@
 function class = classify(c, x)
   done = false;
   i = 1;
-  class = 0;
+  class =0;
   while(~done && i < size(c,1))
     g=c(i,:);
     cls = applyDisc(g, x);
-    if cls == 1 && g(4) == 0
+    if cls == 1 && g(5) == 0
       class = cls;
       done = true;
-    elseif cls == 2 && g(5) == 0
+    elseif cls == 2 && g(4) == 0
       class = cls;
       done = true;
     end
